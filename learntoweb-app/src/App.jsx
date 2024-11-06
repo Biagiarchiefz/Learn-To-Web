83fb5a87748c4e4e9ec97c5954d7a5d5ba0d5b97
Biagiarchiefz <biagiiarchie@gmail.com>
2024-11-06 10:43:14 +0700

    hero section

--- learntoweb-app/src/App.jsx
+++ learntoweb-app/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar/Navbar"
 
 function App() {
   return <div className="bg-white text-dark overflow-x-hidden">
-    <Navbar />
+  
     <Hero/>
   </div>;
 }
--- learntoweb-app/src/components/Hero/Hero.jsx
+++ learntoweb-app/src/components/Hero/Hero.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import { IoIosArrowRoundForward } from "react-icons/io";
+import hero from "../../assets/hero.png"
 
 const Hero = () => {
   return (
-    <section>
-      <div className="container">
+    <section className='bg-light overflow-hidden relative'>
+      <Navbar/>
+      <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[650px]">
+
+    
+        {/* text hero */}
+        <div className="flex flex-col justify-center py-14 md:py-0  relative z-20">
+          <div className="text-center md:text-left space-y-10 lg:max-w-[400px]">
+          <h1 className='text-3xl lg:text-5xl font-bold leading-snug'>Let's Learn to build a <span className='text-secondary'>website </span> 
+          for your bussiness 
+          </h1>
+
+          <div className="flex justify-center md:justify-start">
+          <button className='primary-btn flex items-center gap-2 group'>
+            Let's Join
+            <IoIosArrowRoundForward className='text-xl group-hover:translate-x-2
+            group-hover:-rotate-45 duration-300' />
+            </button>
+          </div>
+          </div>
+
+        </div>
+
+        {/* hero image */}
+        <div className="flex flex-col justify-center">
+          
+          <img src={hero} alt="" />
+          <h1>ssa</h1>
+
+        </div>
+
+ 
+
+
         
       </div>
 
