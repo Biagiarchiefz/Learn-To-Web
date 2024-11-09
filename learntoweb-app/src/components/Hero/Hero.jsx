dfdee1e16f9897e2fa27ea570e21a0611923a3f2
Biagiarchiefz <biagiiarchie@gmail.com>
2024-11-09 22:47:12 +0700

    services

--- learntoweb-app/src/components/Hero/Hero.jsx
+++ learntoweb-app/src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import hero from "../../assets/hero.png"
 import { animate, delay, easeInOut, motion } from 'framer-motion'
 
 
-export const fadeup = () => {
+export const Fadeup = (delay) => {
   return {
     initial : {
       opacity : 0,
@@ -15,7 +15,7 @@ export const fadeup = () => {
     animate : {
       opacity : 1,
       y : 0,
-      trasition : {
+      transition : {
         type : "spring",
         stiffness : 100,
         duration : 0.5,
@@ -38,20 +38,24 @@ const Hero = () => {
         <div className="flex flex-col justify-center py-14 md:py-0  relative z-20">
           <div className="text-center md:text-left space-y-10 lg:max-w-[400px]">
           <motion.h1 
-          variants={fadeup(0.6)} 
+          variants={Fadeup(0.6)} 
           initial="initial"
           animate= "animate"
           className='text-3xl lg:text-5xl font-bold leading-snug'>Let's Learn to build a <span className='text-secondary'>website </span> 
           for your bussiness 
           </motion.h1>
 
-          <div className="flex justify-center md:justify-start">
+          <motion.div
+          variants={Fadeup(0.8)}
+          initial="initial"
+          animate= "animate"
+           className="flex justify-center md:justify-start">
           <button className='primary-btn flex items-center gap-2 group'>
             Let's Join
             <IoIosArrowRoundForward className='text-xl group-hover:translate-x-2
             group-hover:-rotate-45 duration-300' />
             </button>
-          </div>
+          </motion.div>
           </div>
 
         </div>
@@ -59,7 +63,11 @@ const Hero = () => {
         {/* hero image */}
         <div className="flex flex-col justify-center">
           
-          <img src={hero} alt=""className='w-[400px] lg: w-[600px] drop-shadow'/>
+          <motion.img
+          initial = {{ x: 50, opacity: 0 }}
+          animate = {{ x: 0, opacity: 1 }}
+          transition = {{duration: 0.6, delay: 0.4, ease: easeInOut}}
+          src={hero} alt=""className='w-[400px] lg: w-[600px] drop-shadow'/>
         
         </div>
 
--- learntoweb-app/src/components/Services/Services.jsx
+++ learntoweb-app/src/components/Services/Services.jsx
@@ -1,39 +1,77 @@
-import React from 'react'
+import { delay } from "framer-motion";
+import React from "react";
 import { TbWorldWww } from "react-icons/tb";
+import {motion} from 'framer-motion'
 
-const services = [
+const servicesData = [
   {
     id: 1,
-    title : 'web development'
+    title: "web development",
+    link: "#",
+    icons: <TbWorldWww />,
+    delay: 0.2,
+  },
+  {
+    id: 2,
+    title: "web development",
+    link: "#",
+    icons: <TbWorldWww />,
+    delay: 0.3,
+  },
+  {
+    id: 3,
+    title: "web development",
+    link: "#",
+    icons: <TbWorldWww />,
+    delay: 0.4,
+  },
+  {
+    id: 4,
+    title: "web development",
+    link: "#",
+    icons: <TbWorldWww />,
+    delay: 0.5,
+  },
+  {
+    id: 5,
+    title: "web development",
+    link: "#",
+    icons: <TbWorldWww />,
+    delay: 0.6,
+  },
+  {
+    id: 6,
+    title: "web development",
+    link: "#",
+    icons: <TbWorldWww />,
+    delay: 0.7,
   }
-]
+];
 
 const Services = () => {
   return (
-    <div className='container'>
-      <div className="">
-      <h1 className='text-4xl font-bold text-secondary py-10'>Service we provide</h1>
-      </div>
+    <section className="bg-white">
+      <div className="container pb-14 pt-16">
+        <h1 className="font-bold text-4xl pb-10">Service we provide</h1>
 
-      <div className="">
-        <div className="w-[190px] h-[160px] bg-dark2 rounded-[10%] flex flex-col justify-center items-center gap-8">
-        <TbWorldWww className='text-4xl'/>
-        
-      <div className="">
-        
-        { 
-          services.map((item) => (
-            item.title
-          ))
-        }
-      
-      </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-8">
+          {servicesData.map((item) => (
+            <div className="bg-[#f4f4f4] rounded-2xl flex flex-col justify-center items-center gap-4 p-4 py-7 hover:bg-white hover:shadow-2xl hover:scale-110 duration-300">
+
+              <div className="text-4xl mb-4">{item.icons}</div>
+
+              
+              <h1 className="text-lg text-center font-semibold px-3 w-[170px]">
+                {item.title}
+              </h1>
+
+            </div>
+          ))}
+
         </div>
-
       </div>
-      
-    </div>
-  )
-}
+    </section>
+  );
+};
 
-export default Services
+export default Services;
